be5e02f7a57729e9f5b437799ac7517777e5b08e
Theodore J. Perkins <42140573+theodorejperkins@users.noreply.github.com>
2019-01-17 10:02:21 -0500

    Add files via upload

--- RECAP.pl
+++ RECAP.pl
@@ -21,6 +21,12 @@
 #   05/29/2018 - v1.0.8 - Saves output to separate bootstrapped files
 #   08/24/2018 - v1.1.0 - Major bugfix to RECAP calculation with tied p-values
 #   08/28/2018 - v1.1.1 - Relative path fix when using ./
+#   11/18/2018 - v1.2.0 - Linear interpolation of RECAP p-values to remove duplicates
+#   11/20/2018 - v1.2.1 - Linear interpolation changes
+#   11/21/2018 - v1.2.2 - More linear interpolation changes
+#   12/24/2018 - v1.2.3 - Funky distribution tail fixer (pseudocount re-mixed p-value)
+#   01/14/2019 - v1.2.4 - Remove all LFDR and no bootstrap folder
+#   01/16/2019 - v1.2.5 - Better instructions
 #
 # CREDITS:
 # RECAP was developed by Justin G. Chitpin, Aseel Awdeh, and Theodore J. Perkins.
@@ -38,7 +44,7 @@
 
 # ==================================================================
 # Version
-our $version = "1.1.1";
+our $version = "1.2.5";
 
 # Modules
 use warnings;
@@ -47,6 +53,7 @@ use Getopt::Long;
 use Term::ANSIColor qw(:constants);
 use List::BinarySearch qw(binsearch_pos);
 use List::Util qw(min max);
+use List::MoreUtils qw(uniq);
 use Math::Utils qw(ceil floor);
 use autodie;
 $| = 1;
@@ -60,8 +67,6 @@ BEGIN { our $start_run = time(); }
 
 # Variables
 my @remixList;    # Holds the names of bootstrapped re-mixed files
-my $importOrig;   # Placeholder
-my $importRemix;  # Placeholder
 my @fileOriginal; # Holds contents of original file
 my @fileRemix;    # Holds contents of re-mixed file
 my @fileRemix2;   # Holds contents of multiple re-mixed files
@@ -69,28 +74,11 @@ my @columnsOriginal; # Holds contents of original file split by delimiter
 my @columnsRemix;    # Holds contents of original file split by delimiter
 my @pvalOrig;    # Original p-values
 my @pvalRemix;   # Re-mixed p-values
-my $matching;    # Number of re-mixed p-values equal or less than original p
 my @RECAP;       # Re-calibrated r-values
 my @fileHeader;  # File header for output file
 my $delimOutput; # Delimiter type passed
 my @indexFDR;    # Index of FDR values corresponding to original file
 my @joinFDR;     # FDR values indexed by $indexFDR
-my @RECAPhist;   # Re-calibrated r-values
-my @binsLog;     # -log histogram bin edges. ex. 0:1 -> 1:0.1
-my @bins;        # raw histogram bin edges. ex. 0:1 -> 1:0.1
-my @histCount;   # Histogram count of the LFDR values by @bins
-my @theoreticalBins; # Theoretical histogram count of LFDR values by @bins
-my @LFDR;        # LFDR values calculated by histogram based estimation
-my $dirBoot;     # Bootstrap folder to save the output
-
-# Unused variables for estimating the LFDR
-=pod
-my $KDE;         # Calculator object
-my $LFDRKDE;     # Smoothed LFDR values by KDE method
-my $wBandwidth;  # Bandwidth of KDE
-my $minKDE;      # Minimum LFDR value for KDE
-my $maxKDE;      # Maximum LFDR value for KDE
-=cut
 
 # Arguments
 GetOptions(
@@ -120,17 +108,18 @@ if ( $help || ! defined $dirOriginal
            || ! defined $delim
            || ! defined $software) {
   print BOLD, "  USAGE:\n", RESET;
-  print "  --dirOrig\tOriginal file directory\n";
-  print "  --nameOrig\tOriginal file name without\n";
-  print "  --dirRemix\tRe-mixed file directory\n";
+  print "  --dirOrig\tOriginal file directory (absolute)\n";
+  print "  --nameOrig\tOriginal file name\n";
+  print "  --dirRemix\tRe-mixed file directory (absolute)\n";
   print "  --nameRemix\tRe-mixed file name without '.bootstrap_#.bed\n";
-  print "  --dirOutput\tOutput file directory\n";
+  print "  --dirOutput\tOutput file directory (absolute)\n";
   print "  --nameOutput\tOutput file name\n";
   print "  --bootstrap\tNumber of re-mixing procedures\n";
   print "  --header\tHeader lines in files\n";
   print "  --pvalCol\tColumn containing p-values\n";
   print "  --delim\tDelimiter type of file (t)ab or (c)omma\n";
-  print "  --software\tPeak caller (M)ACS2 or (D)iffReps or (O)ther\n";
+  print "  --software\tPeak caller (M)ACS2 or (D)iffReps or (O)ther\n\n";
+  print BOLD, "  OPTIONS:\n", RESET;
   print "  --help\tDisplay this help and exit\n";
   exit;
 }
@@ -140,11 +129,11 @@ print "##################################################\n";
 print "############       RECAP v$version       ############\n";
 print "##################################################\n";
 print "\n";
-print "Input directory (Original): $dirOriginal\n";
-print "Input file name (Original): $nameOriginal\n";
-print "Input directory (Re-Mixed): $dirRemix\n";
-print "Input file name (Re-Mixed): $nameRemix\n";
-print "Output directory: $dirOutput\n";
+print "Input directory  (Original; absolute path): $dirOriginal\n";
+print "Input file name  (Original): $nameOriginal\n";
+print "Input directory  (Re-mixed; absolute path): $dirRemix\n";
+print "Input file name  (Re-mixed): $nameRemix\n";
+print "Output directory (Absolute path): $dirOutput\n";
 print "Output file name: $nameOutput\n";
 print "Bootstrap number: $bootstrap\n";
 print "Header number   : $header\n";
@@ -220,7 +209,7 @@ if ( $software =~ /[^MDO]/ ) {
 print "\n";
 print "Reading original summary file\n";
 chdir($dirOriginal);
-open($importOrig, '<', $nameOriginal);
+open(my $importOrig, '<', $nameOriginal);
 while(<$importOrig>) {
 	next unless $. > $header;
 	push @fileOriginal, $_;
@@ -240,7 +229,7 @@ closedir $DIR;
 
 # Pushing all re-mixed datasets into one array
 for ( my $i=0; $i<$bootstrap; $i++ ) {
-	open($importRemix, '<', $remixList[$i]);
+	open(my $importRemix, '<', $remixList[$i]);
 	while(<$importRemix>) {
 		next unless $. > $header;
 		push @fileRemix2, $_;
@@ -280,18 +269,72 @@ foreach my $line (@fileRemix) {
 }
 print "Check!\n";
 
-# Sort re-mixed p-values
-print "Sorting re-mixed p-values\n";
-my @sorted_pvalRemix = sort { $b <=> $a } @pvalRemix;
-print "Check!\n";
-
 ## RECAP algorithm
-print "Recalibrating via RECAP procedure\n";
-
 # Implementing RECAP algorithm with binary search
-for my $idx ( 0 .. $#pvalOrig ) {
-	$matching = binsearch_pos { $b <=> $a } $pvalOrig[$idx], @sorted_pvalRemix;
-	$RECAP[$idx] = ((scalar @sorted_pvalRemix) - $matching) / scalar @sorted_pvalRemix;
+{
+	# Sort re-mixed p-values
+	print "Sorting re-mixed p-values\n";
+	my @sorted_pvalRemix = @pvalRemix;
+	push @sorted_pvalRemix, 0;
+	@sorted_pvalRemix = sort { $b <=> $a } @sorted_pvalRemix;
+	print "Check!\n";
+
+	print "Recalibrating via RECAP procedure\n";
+	for my $idx ( 0 .. $#pvalOrig ) {
+		# Number of re-mixed p-values equal or less than original p
+		my $matching = binsearch_pos { $b <=> $a } $pvalOrig[$idx], @sorted_pvalRemix;
+		$RECAP[$idx] = ((scalar @sorted_pvalRemix) - $matching) / scalar @sorted_pvalRemix;
+	}
+	print "Check!\n";
+}
+
+# Linear interpolation of RECAP p-values
+print "Linear interpolation of RECAP p-values based on original and re-mixed p-values\n";
+{
+	# Sorting re-mixed p-values and saving a vector of unique values
+	my @sorted_pvalRemix = @pvalRemix;
+	push @sorted_pvalRemix, 0;
+	@sorted_pvalRemix = sort { $b <=> $a } @sorted_pvalRemix;
+	my @sorted_pvalRemix2 = @pvalRemix;
+	push @sorted_pvalRemix2, 0;
+	@sorted_pvalRemix2 = sort { $a <=> $b } @sorted_pvalRemix;
+
+	#my @sorted_pvalRemix = sort { $b <=> $a } @pvalRemix;
+	#my @sorted_pvalRemix2 = sort { $a <=> $b } @pvalRemix;
+	my @sorted_pvalRemix_uniq = uniq @sorted_pvalRemix2;
+
+	# Initialize linear interpolated RECAP p-values
+	my @liRECAP;
+
+	# Linear interpolation code begins here
+	for my $idx ( 0 .. $#pvalOrig ) {
+		my $matching = binsearch_pos { $a <=> $b } $pvalOrig[$idx], @sorted_pvalRemix_uniq;
+		my $r2 = $sorted_pvalRemix_uniq[$matching];   # the bigger re-mixed p-value vs $pvalOrig[$idx]
+		my $r1 = $sorted_pvalRemix_uniq[$matching-1]; # the smaller re-mixed p-value vs $pvalOrig[$idx]
+		if ( $pvalOrig[$idx] < $sorted_pvalRemix_uniq[0] ) {
+			my $r1Matching = binsearch_pos { $b <=> $a } $sorted_pvalRemix_uniq[0], @sorted_pvalRemix;
+			my $r1RECAP = (( scalar @sorted_pvalRemix) - $r1Matching) / scalar @sorted_pvalRemix;
+			my $temp = $pvalOrig[$idx]/$sorted_pvalRemix_uniq[0] * $r1RECAP;
+			push @liRECAP, $temp;
+		} elsif ( defined $r2 && $pvalOrig[$idx] == $r2) {
+			push @liRECAP, $RECAP[$idx];
+		} elsif ( defined $r2 && $r1 <= $pvalOrig[$idx] && $pvalOrig[$idx] <= $r2 ) {
+			my $r2Matching = binsearch_pos { $b <=> $a } $r2, @sorted_pvalRemix;
+			my $r2RECAP = (( scalar @sorted_pvalRemix) - $r2Matching) / scalar @sorted_pvalRemix;
+			my $r1Matching = binsearch_pos { $b <=> $a } $r1, @sorted_pvalRemix;
+			my $r1RECAP = (( scalar @sorted_pvalRemix) - $r1Matching) / scalar @sorted_pvalRemix;
+			my $temp = ($pvalOrig[$idx] - $r1)/($r2-$r1) * ($r2RECAP - $r1RECAP) + $r1RECAP;
+			push @liRECAP, $temp;
+		} elsif ( $pvalOrig[$idx] > $sorted_pvalRemix_uniq[$#sorted_pvalRemix_uniq] ) {
+			push @liRECAP, 1;
+		} else {
+			print("$pvalOrig[$idx] > $sorted_pvalRemix_uniq[$#sorted_pvalRemix_uniq] || $r1 <= $pvalOrig[$idx] <= $r2");
+			die("Linear interpolation failed. Check line 313 in the source code.");
+		}
+	}
+	# Overwrite original RECAP p-values with the interpolated ones
+	@RECAP = @liRECAP;
+	print "Check!\n";
 }
 
 ## Benjamini-Hochberg adjustment of RECAP r-values
@@ -327,111 +370,32 @@ for ( my $i=$#joinFDR; $i>0; $i-- ) {
 # Only retain column of FDR r-values
 @joinFDR = map{ $$_[1] } @joinFDR;
 
-## Calculating the LFDR using a simple histogram-based estimation approach
-print "Calculating the LFDR\n";
-
-# Removing all r-values equal to 0. This is because the LFDR cannot
-# be calculated from r-values equal to 0.
-foreach ( @RECAP ) {
-	push @RECAPhist, $_ if ($_) != 0;
-}
-
-# Calculate optimal histogram bin ranges
-{
-	my $max = max(@RECAPhist);
-	my $min = min(@RECAPhist);
-	$max = floor(-(log($max) / log(10)));
-	$min = ceil(-(log($min) / log(10)));
-
-	for ( my $i=$max; $i<=$min; $i=$i+0.5 ) { # CHANGED to half decade
-		push @binsLog, $i;
-	}
-	# Raw half decade bin edges (ex. 1.0, 0.316, 0.1, 0.0316 ...)
-	@bins = map { 10**(-$_) } @binsLog;
-}
-
-# Histogram count by binary searching index
-{
-	# Sort r-values for histogram counting
-	my @RECAPsort = sort { $a <=> $b } @RECAPhist;
-	my $index;
-	#for my $i ( 0 .. ($#bins-1) ) {
-	for ( my $i=0; $i<(scalar @bins - 1); $i++ ) {
-		$index = (binsearch_pos {$a <=> $b} $bins[$i], @RECAPsort) - (binsearch_pos {$a <=> $b} $bins[$i+1], @RECAPsort);
-		$histCount[$i] = $index / (scalar @RECAPsort);
-	}
-}
-
-# Calculating theoretical bins under uniform distribution
-{
-	for ( my $i=0; $i<(scalar @bins - 1); $i++ ) {
-		$theoreticalBins[$i] = $bins[$i] - $bins[$i+1];
-	}
-}
-
-# Generate theoretical histogram under uniform distribution
-# Calculate the local false discovery rate. Uniform(p) / pdf(RECAP)
-# Histogram-based estimation
-{
-	#for $i ( 0 .. ($#bins-1) ) {
-	for ( my $i=0; $i<(scalar @theoreticalBins); $i++ ) {
-		unless ( $histCount[$i] == 0 ) {
-			$LFDR[$i] = $theoreticalBins[$i] / $histCount[$i];
-		} else {
-			$LFDR[$i] = 0;
-		}
-		if ( $LFDR[$i] > 1 ) {
-			$LFDR[$i] = 1;
-		}
-	}
-}
-
-# Assigning LFDR value to each row of RECAP r-values
-my @LFDRorder;
-for my $element (0 .. $#RECAP) {
-	my $j=0;
-	while ( $RECAP[$element] != 0 && $bins[$j] > $RECAP[$element] && $RECAP[$element] <= $bins[$j+1] ) {
-		$j++;
-	}
-	unless ( $RECAP[$element] == 0 ) {
-		push ( @LFDRorder, $LFDR[$j] );
-	} else {
-		push ( @LFDRorder, 1 );
-	}
-}
-
 # Save recalibrated p-values to file
 print "Saving RECAP summary file to output\n";
 
 # Saving header from original file and adding RECAP BH(RECAP) column headers
 if ( $header != 0 ) {
   chdir($dirOriginal);
-  open($importOrig, '<', $nameOriginal);
+  open(my $importOrig, '<', $nameOriginal);
   while(<$importOrig>) {
     next unless $. <= $header;
     push @fileHeader, $_;
   }
 chomp($fileHeader[$header-1]);
-push @fileHeader, "${delimOutput}RECAP${delimOutput}BH(RECAP)${delimOutput}LFDR\n";
+push @fileHeader, "${delimOutput}RECAP${delimOutput}BH(RECAP)${delimOutput}\n";
 }
 
 # Create a new folder called "bootstrap_#" and save output to that folder
-$dirBoot = "bootstrap_$bootstrap";
 chdir($dirOutput);
-unless( -d $dirBoot ) {
-  mkdir $dirBoot;
-}
-chdir("$dirOutput/$dirBoot");
 open ( my $exportRECAP, ">", $nameOutput );
 print $exportRECAP join "", @fileHeader;
 
 # Add column of r-values to input, original summary file
 # Add column of Benjamini-Hochberg corrected r-value
-# Add column of LFDR values
 for my $idx ( 0 .. $#fileOriginal ) {
   @columnsOriginal = split(/$delim/, "$fileOriginal[$idx]");
   chomp(@columnsOriginal);
-  push @columnsOriginal, $RECAP[$idx], $joinFDR[$idx], $LFDRorder[$idx], "\n";
+  push @columnsOriginal, $RECAP[$idx], $joinFDR[$idx], "\n";
   print $exportRECAP join $delimOutput, @columnsOriginal;
 }
 close $exportRECAP;
